a799664dcf1ef17aea7b200432a973d708fae09a
milsling <picconetrevor@gmail.com>
2026-02-26 23:47:40 +0000

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './client/src/**/*.{js,ts,jsx,tsx}',
+    './client/public/index.html',
+    './shared/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
